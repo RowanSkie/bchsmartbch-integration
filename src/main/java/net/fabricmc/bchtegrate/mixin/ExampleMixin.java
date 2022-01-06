b9bdb16cfbca605e167bd995e28bcf5e2f266550
Mark Joshua Zuniga <markzuniga6@gmail.com>
2022-01-06 08:55:42 +0800

    initial push, rename stuff

--- src/main/java/net/fabricmc/bchtegrate/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/bchtegrate/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.fabricmc.bchtegrate.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.fabricmc.bchtegrate.BCHtegrate;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
